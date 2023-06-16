9716a6bfe63a8ad7fad9901b32ddc1c28b805623
silvermachine777 <sebastian_gg@outlook.com>
2023-06-16 09:58:39 -0500

    feat(environment): Add apiUrlByTranches, apiUrlByClient and apiUrlTop20 environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    apiUrlByTranches: 'localhost:3000/historicalConsumptions/byTranches',
+    apiUrlByClient: 'localhost:3000/historicalConsumptions/byClient',
+    apiUrlTop20: 'localhost:3000/historicalConsumptions/top20WorstSegmentsByClient',
+  };
