c2c41b9b36fb9ab8e4552ece7b06678e86d336b1
silvermachine777 <sebastian_gg@outlook.com>
2023-06-16 18:23:06 -0500

    feat(modules): Update app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DatepickerComponent } from './components/datepicker/datepicker.component';
 import { TableComponent } from './components/table/table.component';
 import { ShowReportComponent } from './components/show-report/show-report.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TranchesService } from './services/tranches.service';
+import { ClientService } from './services/client.service';
+import { Top20Service } from './services/top20.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,10 @@ import { ShowReportComponent } from './components/show-report/show-report.compon
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TranchesService, ClientService, Top20Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
