2b7ecec68b1ae63af9978c03b10a3fa12f1bf397
silvermachine777 <sebastian_gg@outlook.com>
2023-06-16 18:29:01 -0500

    feat(component): update datepicker component

--- src/app/components/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateStruct, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-datepicker',
@@ -7,11 +7,38 @@ import { NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./datepicker.component.css'],
 })
 export class DatepickerComponent {
-  modelInicial: NgbDateStruct;
-  modelFinal: NgbDateStruct;
+  modelInicial!: NgbDateStruct;
+  modelFinal!: NgbDateStruct;
 
-  constructor() {
-    this.modelInicial = {} as NgbDateStruct;
-    this.modelFinal = {} as NgbDateStruct;
+  fechaInicial!: NgbDateStruct;
+  fechaFinal!: NgbDateStruct;
+
+  constructor(private ngbDateParserFormatter: NgbDateParserFormatter) {}
+
+  onDateSelect(date: NgbDateStruct, inputId: string) {
+    if (inputId === 'dpInicial') {
+      this.modelInicial = date;
+      this.fechaInicial = this.ngbDateParserFormatter.parse(this.ngbDateParserFormatter.format(date))!;
+    } else if (inputId === 'dpFinal') {
+      this.modelFinal = date;
+      this.fechaFinal = this.ngbDateParserFormatter.parse(this.ngbDateParserFormatter.format(date))!;
+    }
   }
+  
+
+  onInputChange(event: Event, inputId: string) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    const parsedDate = this.ngbDateParserFormatter.parse(inputValue);
+    if (parsedDate) {
+      if (inputId === 'dpInicial') {
+        this.modelInicial = parsedDate;
+        this.fechaInicial = parsedDate;
+      } else if (inputId === 'dpFinal') {
+        this.modelFinal = parsedDate;
+        this.fechaFinal = parsedDate;
+      }
+    }
+  } 
+  
+  
 }
