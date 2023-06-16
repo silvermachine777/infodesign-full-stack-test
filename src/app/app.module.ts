15099c44ee92f02c92c9ab084484526995a3c222
silvermachine777 <sebastian_gg@outlook.com>
2023-06-16 10:32:29 -0500

    feat(modules): Add show report component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DatepickerComponent } from './components/datepicker/datepicker.component';
 import { TableComponent } from './components/table/table.component';
+import { ShowReportComponent } from './components/show-report/show-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatepickerComponent,
-    TableComponent
+    TableComponent,
+    ShowReportComponent
   ],
   imports: [
     BrowserModule,
