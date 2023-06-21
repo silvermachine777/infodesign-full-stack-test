41ef476cfc530165b461e8ca67d38a750bc5e860
silvermachine777 <sebastian_gg@outlook.com>
2023-06-20 22:19:37 -0500

    feat(component): Add pagination in table componente

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,19 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Client } from 'src/app/models/client.model';
 import { Top20 } from 'src/app/models/top20.model';
 import { Tranches } from 'src/app/models/tranches.model';
+import { DecimalPipe } from '@angular/common';
+import { NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
-export class TableComponent {
+export class TableComponent implements OnChanges {
   @Input() data!: (Tranches | Client | Top20)[];
+  
+  page = 1;
+  pageSize = 10;
+  collectionSize: number = 0;
+  paginatedData: (Tranches | Client | Top20)[] = [];
+
+  constructor(private decimalPipe: DecimalPipe, private config: NgbPaginationConfig) {
+    config.boundaryLinks = true;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data'] && !changes['data'].firstChange) {
+      this.refreshData();
+    }
+  }
 
   getColumns(): string[] {
-    if (this.data.length > 0) {
-      return Object.keys(this.data[0]);
+    if (this.paginatedData && this.paginatedData.length > 0) {
+      return Object.keys(this.paginatedData[0]);
     }
     return [];
   }
@@ -21,4 +38,18 @@ export class TableComponent {
   getPropertyValue(item: Tranches | Client | Top20, column: string): any {
     return (item as any)[column];
   }
+
+  onPageChange(page: number) {
+    this.page = page;
+    this.refreshData();
+  }
+
+  refreshData() {
+    if (this.data) {
+      const startIndex = (this.page - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      this.collectionSize = this.data.length;
+      this.paginatedData = this.data.slice(startIndex, endIndex);
+    }
+  }  
 }
